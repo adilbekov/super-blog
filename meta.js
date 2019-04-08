c866802405f03734fbe3a1a21af0d68d79efef52
Ramen <ramil@adilbekov.com>
2019-04-08 10:44:20 +0300

    Add files via upload

---
+++ meta.js
@@ -0,0 +1,11 @@
+var main = new Vue({
+    el: '#main',
+    data: {
+        header: 'Adilbekov',
+        cdn_host: 'https://264018.selcdn.ru/cdn01',
+        menu1_name: 'Блог',
+        menu1_url: '/',
+        menu2_name: 'Вкусняшки',
+        menu2_url: '/useful',
+    }
+})
---
+++ post_data.js
@@ -0,0 +1,22 @@
+var post_01 = new Vue({
+    el: '#post_01',
+    data: {
+        cdn_host: 'https://264018.selcdn.ru/cdn01',
+        title: 'Слушаем и храним любимую музыку в Telegram',
+        date: '13.11.2017',
+        image: '/blog/1/main.jpg',
+        author: 'Dev',
+        short: 'В то время как Вконтакте ограничил прослушивание музыки в фоне, Telegram выпустил обновление, в котором обновили музыкальный плеер и теперь слушать музыку можно в любимом мессенджере.'
+    }
+})
+var post_02 = new Vue({
+    el: '#post_02',
+    data: {
+        cdn_host: 'https://264018.selcdn.ru/cdn01',
+        title: 'Смена региона на Gear S3',
+        date: '99.99.2099',
+        image: '/blog/2/main.jpg',
+        author: 'Dev',
+        short: 'В этом посте я расскажу как сменить регион на смарт-часах Gear S3.'
+    }
+})
